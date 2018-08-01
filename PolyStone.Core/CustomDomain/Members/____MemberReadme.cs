ce4a6b8a38bcd274cede733df882baedb96b22b1
JuniorCai <JuniorCai@outlook.com>
2018-08-01 13:47:31 +0800

    完善个人会员信息表相关类

---
+++ PolyStone.Core/CustomDomain/Members/____MemberReadme.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace PolyStone.CustomDomain.Members
+{
+	
+			//TODO:Angular后端的导航栏目设计
+
+	/*
+	//无次级导航属性
+	   var member=new MenuItemDefinition(
+                MemberAppPermissions.Member,
+                L("Member"),
+				url:"members",
+				icon:"icon-grid",
+					 				 requiredPermissionName: MemberAppPermissions.Member
+				  				);
+
+*/
+				//有下级菜单
+            /*
+
+			   var member=new MenuItemDefinition(
+                MemberAppPermissions.Member,
+                L("Member"),			
+				icon:"icon-grid"				
+				);
+
+				member.AddItem(
+			new MenuItemDefinition(
+			MemberAppPermissions.Member,
+			L("Member"),
+			"icon-star",
+			url:"members",
+			 			requiredPermissionName: MemberAppPermissions.Member));
+	
+
+			
+			*/
+}
