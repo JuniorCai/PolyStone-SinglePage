81a5b4a5967c2d0c75554bb93194380cdad38ec0
JuniorCai <JuniorCai@outlook.com>
2018-09-26 11:40:33 +0800

    完善企业添加页图片上传功能

--- PolyStone.Web/App/Main/views/company/createCompany.js
+++ PolyStone.Web/App/Main/views/company/createCompany.js
@@ -4,40 +4,86 @@
         function ($scope, companyService, industryService,regionService,FileUploader) {
             var vm = this;
             var imgUrls = [];
-            vm.uploadResult = {
+            vm.uploadResult1 = {
                 status: true,
                 msg:""
             };
-            
-            var uploader = $scope.fileUploader1 = $scope.fileUploader = new FileUploader({
+            vm.uploadResult2 = {
+                status: true,
+                msg: ""
+            };
+            vm.uploadResult3 = {
+                status: true,
+                msg: ""
+            };
+            vm.uploadResult4 = {
+                status: true,
+                msg: ""
+            };
+
+            var fileUploaderCfg = {
                 url: "/Resource/Upload",
                 queueLimit: 1,
                 headers: {
                     'x-xsrf-token': abp.security.antiForgery.getToken()
                 }
-            });
+            };
 
-            uploader.filters.push({
+            var imageFilter = {
                 name: 'imageFilter',
                 fn: function(item /*{File|FileLikeObject}*/, options) {
                     var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
                     return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
                 }
-            });
-            uploader.filters.push({
+            };
+
+            var sizeFilter = {
                 name: 'sizeFilter',
                 fn: function (item /*{File|FileLikeObject}*/, options) {
-
-                    return item.size <= 1048576;
-                }
-            });
-            uploader.onAfterAddingAll = function() {
-                if (this.queue.length == 1) {
-                    $("#uploadDiv").hide();
-                } else {
-                    $("#uploadDiv").show();
+                    var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                    return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
                 }
             };
+            
+            var fileUploader1 = $scope.logoFileUploader = new FileUploader(fileUploaderCfg);
+            var fileUploader2 = $scope.licenceFileUploader = new FileUploader(fileUploaderCfg);
+            var fileUploader3 = $scope.personFileUploader = new FileUploader(fileUploaderCfg);
+            var fileUploader4 = $scope.nationalFileUploader = new FileUploader(fileUploaderCfg);
+
+
+            fileUploader1.filters.push(imageFilter);
+            fileUploader1.filters.push(sizeFilter);
+
+            fileUploader2.filters.push(imageFilter);
+            fileUploader2.filters.push(sizeFilter);
+
+            fileUploader3.filters.push(imageFilter);
+            fileUploader3.filters.push(sizeFilter);
+
+            fileUploader4.filters.push(imageFilter);
+            fileUploader4.filters.push(sizeFilter);
+
+            fileUploader1.onAfterAddingFile = function() {
+                toggleUploadDiv("#logoUploadDiv", this.queue.length >= 1);
+            };
+            fileUploader2.onAfterAddingFile = function () {
+                toggleUploadDiv("#licenseUploadDiv", this.queue.length >= 1);
+            };
+            fileUploader3.onAfterAddingFile = function () {
+                toggleUploadDiv("#personUploadDiv", this.queue.length >= 1);
+
+            };
+            fileUploader4.onAfterAddingFile = function () {
+                toggleUploadDiv("#nationalUploadDiv", this.queue.length >= 1);
+            };
+
+            function toggleUploadDiv(divId, isHide) {
+                if (isHide) {
+                    $(divId).hide();
+                } else {
+                    $(divId).show();
+                }
+            }
 
             vm.delImg = function() {
                 //$scope.fileUploader
@@ -150,12 +196,15 @@
                     abp.notify.error("需上传至少一张产品图片");
                     return;
                 }
-                uploader.uploadAll();
+                fileUploader1.uploadAll();
+                fileUploader2.uploadAll();
+                fileUploader3.uploadAll();
+                fileUploader4.uploadAll();
 
             };
 
 
-            uploader.onSuccessItem = function (item, response, status, headers)
+            fileUploader1.onSuccessItem = function (item, response, status, headers)
             {
                 vm.uploadResult.status = response.result.success;
                 vm.uploadResult.msg = response.result.success ? response.result.msg : item.name + " "+response.result.msg;
@@ -165,25 +214,46 @@
                     imgUrls.push(response.result.msg);
                 }
             };
-
-            uploader.onErrorItem = function(item, response, status, headers) {
-                vm.uploadResult.status = false;
-                vm.uploadResult.msg = item.name + "上传失败";
-                vm.fileUploader.cancelAll();
+            fileUploader2.onSuccessItem = function (item, response, status, headers) {
+                vm.uploadResult.status = response.result.success;
+                vm.uploadResult.msg = response.result.success ? response.result.msg : item.name + " " + response.result.msg;
+                if (!vm.uploadResult.status)
+                    vm.fileUploader.cancelAll();
+                else {
+                    imgUrls.push(response.result.msg);
+                }
+            };
+            fileUploader3.onSuccessItem = function (item, response, status, headers) {
+                vm.uploadResult.status = response.result.success;
+                vm.uploadResult.msg = response.result.success ? response.result.msg : item.name + " " + response.result.msg;
+                if (!vm.uploadResult.status)
+                    vm.fileUploader.cancelAll();
+                else {
+                    imgUrls.push(response.result.msg);
+                }
+            };
+            fileUploader4.onSuccessItem = function (item, response, status, headers) {
+                vm.uploadResult.status = response.result.success;
+                vm.uploadResult.msg = response.result.success ? response.result.msg : item.name + " " + response.result.msg;
+                if (!vm.uploadResult.status)
+                    vm.fileUploader.cancelAll();
+                else {
+                    imgUrls.push(response.result.msg);
+                }
             };
 
-            uploader.onCompleteAll = function () {
-
-                if (vm.uploadResult.status) {
-                    vm.product.imgUrls = imgUrls.join(',');
-                    companyService.createCompany(vm.product)
-                        .then(function() {
-                            abp.notify.success(App.localize('SavedSuccessfully'));
-                        });
-                } else {
-                    abp.notify.error(vm.uploadResult.msg);
-                }                
-            }
+//            uploader.onCompleteAll = function () {
+//
+//                if (vm.uploadResult.status) {
+//                    vm.product.imgUrls = imgUrls.join(',');
+//                    companyService.createCompany(vm.product)
+//                        .then(function() {
+//                            abp.notify.success(App.localize('SavedSuccessfully'));
+//                        });
+//                } else {
+//                    abp.notify.error(vm.uploadResult.msg);
+//                }                
+//            }
 
             vm.cancel = function () {
             };
