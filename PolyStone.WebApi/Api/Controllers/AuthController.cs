06e74064ab3d079df4361f0adceb9d1f913b19c8
JuniorCai <JuniorCai@outlook.com>
2018-10-28 21:50:30 +0800

    添加验证API控制器

---
+++ PolyStone.WebApi/Api/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Abp.Web.Models;
+using Abp.WebApi.Controllers;
+
+namespace PolyStone.Api.Controllers
+{
+    public class AuthController : AbpApiController
+    {
+        // GET
+        public async Task<AjaxResponse> CheckUserValid(string loginCode)
+        {
+            return
+            View();
+        }
+    }
+}
