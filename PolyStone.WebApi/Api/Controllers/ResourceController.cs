62d80c0ad0a0b7217eff9c83360c6475a80144b1
JuniorCai <JuniorCai@outlook.com>
2018-11-20 14:42:38 +0800

    添加图片上传处理方法

---
+++ PolyStone.WebApi/Api/Controllers/ResourceController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Abp.Web.Models;
+using PolyStone.Web.Helpers;
+
+namespace PolyStone.Api.Controllers
+{
+    public class ResourceController : PolyStoneApiControllerBase
+    {
+        // GET
+        [HttpPost]
+        public async Task<AjaxResponse> Upload()
+        {
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                return new AjaxResponse(new ErrorInfo("无效请求"));
+            }
+
+            string saveTempPath = 
+                        ImgUploadHelper uploadHelper = new ImgUploadHelper(Request.Files, Server.MapPath("/"));
+            //            var result = uploadHelper.UploadImg();
+            //
+            //            string msg = result.Item1 == ImageUploadStatus.Success ? (uploadHelper.FileServer + result.Item2) : result.Item2;
+
+
+            return new AjaxResponse();
+            //return Json(new { success = result.Item1 == ImageUploadStatus.Success, msg = msg });
+        }
+    }
+}
